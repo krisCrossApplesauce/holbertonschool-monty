b09d303de15259f15fec34f08da0894c3f9e543e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-03-31 13:27:31 -0700

    getting started

---
+++ main.c
@@ -0,0 +1,85 @@
+#include "monty.h"
+
+FILE *file = NULL;
+char *tok = NULL;
+
+/**
+ * check_op - runs the function that corresponds with the command
+ * specified in the current line of the given file
+ *
+ * @tok: contents of the current line in the given file
+ * @stack: doubly linked list, the stack used by the commands
+ * @ln: unsigned int, line number
+ *
+ * Return: void
+ */
+void check_op(char *tok, stack_t **stack, unsigned int ln)
+{
+	int i = 0;
+	instruction_t stuff[] = {
+		{ "push", _push },
+		{ "pall", _pall },
+		{ "pint", _pint },
+		{ "pop", _pop },
+		{ "swap", _swap },
+		{ "add", _add },
+		{ "nop", _nop }
+	};
+
+	while (i < 7)
+	{
+		if (*stuff[i].opcode == tok)
+		{ return (stuff[i].f(stack, ln)); }
+
+		i++;
+	}
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", ln, tok);
+	free(stack);
+	fclose(file);
+	exit(EXIT_FAILURE);
+}
+
+
+/**
+ * main - runs the monty interpreter
+ *
+ * argc: int, number of arguments
+ * argv: str, the arguments
+ *
+ * Return: int, 0 if succeed, 1 if fail
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+	char *line_buff = NULL;
+	size_t buff_size = 0;
+	unsigned int line_num = 1;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+
+	if (!file_contents)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline(&line_buff, &buff_size, file) != EOF)
+	{
+		tok = strtok(line_buff, " \t\n");
+		free(line_buff);
+		line_buff = NULL;
+		check_op(&stack, line_num);
+	}
+
+	free(line_buff);
+	fclose(file);
+	free_stack(&stack);
+	return (EXIT_SUCCESS);
+}
---
+++ op_funcs_1.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack
+ *
+ * @stack: stack_t **, head of doubly linked list
+ * @line_number: unsigned int, number of the current line of file
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	/** add new node at head so then print from head to tail */
+}
+
+
+/**
+ * pall - prints all the values on the stack,
+ * starting from the top of the stack
+ *
+ * @stack: stack_t **, head of doubly linked list
+ * @line_number: unsigned int, number of the current line of file
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+}
